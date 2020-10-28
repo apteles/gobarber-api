f0bc9777129527d8434742c5260b5c7ef00cc16d
Andre Teles <andre.telestp@gmail.com>
2020-10-27 21:14:41 -0300

    chore: add rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -6,12 +6,13 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import configUpload from '@config/upload';
 import globalHandler from '@shared/handlers/globalHandler';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import routes from './routes';
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
-
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(configUpload.uploadsPath));
